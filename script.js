cc5387e1ac77eb302adba0aa9a9edd618add9cbf
Anton Kalashnikov <kalashnikov004@gmail.com>
2023-05-27 23:05:56 +0200

    A - Started working on obtaining and saving data to local storage / db

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
 const apiKey = '83037af74791252875f34544e141853a'; 
-const city = 'London'; // Replace with the desired location
+let city = ''; // Replace with the desired location
 
 const weatherElement = document.getElementById('weather');
 
-// Function to fetch weather data from the API
-const fetchWeatherData = () => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-        .then(response => response.json())
-        .then(data => {
-            const temperature = data.main.temp;
-            const condition = data.weather[0].description;
-            weatherElement.innerHTML = `<p>Location: ${city}</p><p>Temperature: ${temperature}°C</p><p>Condition: ${condition}</p>`;
-        })
-        .catch(error => console.log(error));
-};
 
 // Call the function initially
+// Check if user has saved city in local storage -> yes : set city from local storage; no : try to obtain data from database -> yes : get city from the db no : set city prague automatically
+getCity();
 fetchWeatherData();
 
 // Function to fetch weather data at an interval
-const fetchWeatherDataInterval = () => {
+function fetchWeatherDataInterval() {
     // Check if the previous API call has completed
     if (!fetchWeatherDataInterval.isCallingAPI) {
         // Set the flag to prevent concurrent API calls
@@ -33,6 +24,51 @@ const fetchWeatherDataInterval = () => {
             fetchWeatherDataInterval.isCallingAPI = false;
         }, 60000); // 60 seconds delay
     }
+};
+
+// Function to make an AJAX request
+function fetchDataFromServer() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'api.php', true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        var response = JSON.parse(xhr.responseText);
+        // Process the response data
+        console.log(response);
+      }
+    };
+  
+    xhr.send();
+}
+
+function dbHasCity() {
+    return false;
+}
+
+function getCity() {
+    // Check if user has saved city in local storage -> yes : set city from local storage; no : try to obtain data from database -> yes : get city from the db no : set city prague automatically
+    if (localStorage.getItem('city') != null) {
+        city = localStorage.getItem('city');
+        return;
+    }
+    if (dbHasCity()) {
+        city = getCityFromDb();
+        return;
+    }
+    city = 'Prague';
+    return;
+}
+
+// Function to fetch weather data from the API
+function fetchWeatherData() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        .then(response => response.json())
+        .then(data => {
+            const temperature = data.main.temp;
+            const condition = data.weather[0].description;
+            weatherElement.innerHTML = `<p>Location: ${city}</p><p>Temperature: ${temperature}°C</p><p>Condition: ${condition}</p>`;
+        })
+        .catch(error => console.log(error));
 };
 
 // Call the function every 60 seconds
